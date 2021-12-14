7a682c32abe59d0ddb030a1cf1a839f8e3199592
Kristijan Turic <kristijanyo@gmail.com>
2021-12-14 20:13:12 +0100

    Finished DZ3, copied Weather.cs from before, added all the funcionality to the classes, ForecastUtilties has only one function, was not copied from before; Printer.cs only has printing functionality while the rest is in WeatherGenerator.cs, Program.cs only has the Demo function.

---
+++ DZ3/Zadatak 3/Class Library/ForecastUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Library
+{
+    public class ForecastUtilities
+    {
+        public static void PrintWeathers(IPrinter[] printers, Weather[] weathers)
+        {
+            for(int i = 0; i < weathers.Length; i++)
+            {
+                printers[0].PrintWeather(weathers[i]);
+                printers[1].PrintWeather(weathers[i]);
+            }
+        }
+    }
+}
---
+++ DZ3/Zadatak 3/Class Library/Printer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Class_Library
+{
+    public interface IPrinter
+    {
+        public void PrintWeather(Weather weather);
+    }
+
+    public class ConsolePrinter : IPrinter
+    {
+        private ConsoleColor consoleColor;
+        public ConsolePrinter(ConsoleColor consoleColor)
+        {
+            this.consoleColor = consoleColor;
+            
+        }
+
+        public void PrintWeather(Weather weather)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(weather.ToString());
+        }
+    }
+
+    public class FilePrinter : IPrinter
+    {
+        private string path;
+        public FilePrinter(string path)
+        {
+            this.path = path;
+        }
+
+        public void PrintWeather(Weather weather)
+        {
+            File.WriteAllText(path, weather.ToString());
+        }
+
+    }
+}
