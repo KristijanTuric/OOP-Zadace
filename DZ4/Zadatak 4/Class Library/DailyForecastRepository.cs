ff562c45c42f8d0dd2011d27d4a26d64249c68ca
Kristijan Turic <kristijanyo@gmail.com>
2021-12-24 18:05:23 +0100

    Moved the NoSuchDailyWeatherException exception to a separate file Exceptions.cs for better code readability.

--- DZ4/Zadatak 4/Class Library/DailyForecastRepository.cs
+++ DZ4/Zadatak 4/Class Library/DailyForecastRepository.cs
@@ -65,12 +65,7 @@ namespace Class_Library
             }
         }
 
-    }
 
-    public class NoSuchDailyWeatherException : Exception
-    {
-        public NoSuchDailyWeatherException() { }
-        public NoSuchDailyWeatherException(string message) : base(message) { }
-        public NoSuchDailyWeatherException(string message, Exception innerException) : base(message, innerException) { }
+
     }
 }
---
+++ DZ4/Zadatak 4/Class Library/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Library
+{
+    public class NoSuchDailyWeatherException : Exception
+    {
+        public NoSuchDailyWeatherException() { }
+        public NoSuchDailyWeatherException(string message) : base(message) { }
+        public NoSuchDailyWeatherException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
