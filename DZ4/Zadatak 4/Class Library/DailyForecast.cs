c0121e2dac369c7bf60f4b26822e43796106e1ba
Kristijan Turic <kristijanyo@gmail.com>
2021-12-24 18:01:51 +0100

    Added all the functionality for Add() and Remove(), also added DailyForecast.cs from a previous project.

---
+++ DZ4/Zadatak 4/Class Library/DailyForecast.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Class_Library
+{
+    public class DailyForecast
+    {
+        private DateTime day = new DateTime();
+        private Weather weather = new Weather();
+
+        public DailyForecast(DateTime day, Weather weather)
+        {
+            this.day = day;
+            this.weather = weather;
+        }
+
+        CultureInfo culture = new CultureInfo("pt-BR");
+
+        public Weather Weather => weather;
+        public DateTime Day => day;
+
+        public override string ToString()
+        {
+            return $"{day.ToString(culture)}: T={weather.GetTemperature()}°C, w={weather.GetWindSpeed()}km/h, h={weather.GetHumidity()}%";
+        }
+    }
+}
--- DZ4/Zadatak 4/Class Library/DailyForecastRepository.cs
+++ DZ4/Zadatak 4/Class Library/DailyForecastRepository.cs
@@ -1,9 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Class_Library
 {
     public class DailyForecastRepository
     {
+        private List<DailyForecast> dailyForecasts = new List<DailyForecast>();
+        public DailyForecastRepository() {}
 
+
+        // dodajemo nove dnevne prognoze, ako za neki dan vec imamo prognozu i pokusamo dodati novi prognozu na taj isti dan
+        // onda se stara prognoza zamjeni novom
+        public void Add(DailyForecast dailyForecast)
+        {
+            int flag = 0;
+            for (int i = 0; i < dailyForecasts.Count; i++)
+            {
+                if (dailyForecasts[i].Day.Date == dailyForecast.Day.Date)
+                {
+                    dailyForecasts[i] = dailyForecast;
+                    flag = 1;
+                }
+            }
+            if(flag == 0)
+            {
+                dailyForecasts.Add(dailyForecast);
+            }
+        }
+
+        // isto kao prosla, ali umjesto jednog dana, predajemo listu dana
+        public void Add(List<DailyForecast> listDailyForecast)
+        {
+            int flag;
+            for(int i = 0; i < listDailyForecast.Count; i++)
+            {
+                flag = 0;
+                for (int j = 0; j < dailyForecasts.Count; j++)
+                {
+                    if (dailyForecasts[j].Day.Date == listDailyForecast[i].Day.Date)
+                    {
+                        dailyForecasts[j] = listDailyForecast[i];
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    dailyForecasts.Add(listDailyForecast[i]);
+                }
+            }
+        }
+
+        public void Remove(DateTime date)
+        {
+            int flag = 0;
+            for(int i = 0; i < dailyForecasts.Count; i++)
+            {
+                flag = 0;
+                if (dailyForecasts[i].Day.Date == date.Date)
+                {
+                    dailyForecasts.Remove(dailyForecasts[i]);
+                    flag = 1;
+                }
+                if (flag == 0) throw new NoSuchDailyWeatherException($"No daily forecast for {dailyForecasts[i].Weather.ToString()}");
+            }
+        }
+
+    }
+
+    public class NoSuchDailyWeatherException : Exception
+    {
+        public NoSuchDailyWeatherException() { }
+        public NoSuchDailyWeatherException(string message) : base(message) { }
+        public NoSuchDailyWeatherException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
